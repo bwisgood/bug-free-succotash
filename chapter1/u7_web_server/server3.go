56cf2c9951919d1b5914d21422372f5d0a0cde21
bwisgood <857464370@qq.com>
2019-08-29 19:48:47 +0800

    web server

---
+++ chapter1/u7_web_server/server3.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	_, _ = fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		_, _ = fmt.Fprintf(w, "Header[%q]=%q\n", k, v)
+	}
+	_, _ = fmt.Fprintf(w, "HOST=%q\n", r.Host)
+	_, _ = fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		_, _ = fmt.Fprintf(w, "Form[%q] = %q", k, v)
+	}
+
+}
